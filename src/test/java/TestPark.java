13c9b839e722769e66ddeb631d01defbdba2d761
Alex Bazlinton <abazlinton@gmail.com>
2018-08-07 15:50:23 +0100

    add Park test

---
+++ src/test/java/TestPark.java
@@ -0,0 +1,16 @@
+import ThemePark.Attractions.Park;
+import ThemePark.Visitors.Visitor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestPark {
+
+    @Test
+    public void parkIsUnrestrictedByDefault(){
+        Park park = new Park("Park", 1);
+        Visitor visitor = new Visitor(0, 0, 0);
+        assertTrue(park.isAllowedTo(visitor));
+    }
+
+}
